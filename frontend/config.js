ba03acbc647dc946b31d5fcd30afd1189566159a
rkawasaki02 <ryuji.cloud02@gmail.com>
2026-07-27 03:50:50 +0900

    chore: 設定値をconfig.jsに外出ししてリポジトリ管理に変更

---
+++ frontend/config.js
@@ -0,0 +1,7 @@
+window.VG_CONFIG = {
+	userPoolId: 'ap-northeast-1_HPXDrKY5a',
+	clientId: '1ld5act6auc0ongafp3kob4p6r',
+	domain: 'https://ap-northeast-1hpxdrky5a.auth.ap-northeast-1.amazoncognito.com',
+	redirectUri: 'https://videogarage.jp',
+	apiBaseUrl: 'https://6wo64xbz28.execute-api.ap-northeast-1.amazonaws.com'
+};
